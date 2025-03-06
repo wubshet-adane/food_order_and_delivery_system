d13a0460afe017eff5d2e5d4c4a191e49d5271af
wubshet <0953895891w@gmail.com>
2025-03-06 15:42:42 +0300

    adding login page

--- views/customers/menu.php
+++ views/customers/menu.php
@@ -62,28 +62,9 @@
     <button id="darkModeToggle">🌙 Dark Mode</button>
 
         <!-- Top Bar -->
-        <div class="top-bar">
-            <!-- Logo -->
-            <div class="logo">
-                <a href="index.php" style="color: white; text-decoration: none;">🍴 FoodOrder</a>
-            </div>
-
-            <!-- Center Navigation Links -->
-            <div class="nav-links">
-                <a href="index.php">Home</a>
-                <a href="about.php">About</a>
-                <a href="contact.php">Contact</a>
-                <a href="cart.php">Cart 🛒</a>
-                <a href="menu.php">Menu</a>
-            </div>
-
-            <!-- Authentication Links -->
-            <div class="auth-links">
-                <a href="login.php">Login</a>
-                <a href="register.php">Register</a>
-                <a href="logout.php">Logout</a>
-            </div>
-        </div>
+        <?php
+            include "topbar.php";
+        ?>
         <!--search and sort section-->
         <section class="search-sort container">
                 <form id="searchForm" method="GET" class="search-form">
--- views/customers/topbar.php
+++ views/customers/topbar.php
@@ -2,12 +2,12 @@
 <div class="top-bar">
     <!-- Logo -->
     <div class="logo">
-        <a href="index.php" style="color: white; text-decoration: none;">🍴 FoodOrder</a>
+        <a href="home.php" style="color: white; text-decoration: none;"><img src="../../public/images/logo.jpg" alt="G3 FoodOrder" sizes="" srcset=""></a>
     </div>
 
     <!-- Center Navigation Links -->
     <div class="nav-links">
-        <a href="index.php">Home</a>
+        <a href="home.php">Home</a>
         <a href="about.php">About</a>
         <a href="contact.php">Contact</a>
         <a href="cart.php">Cart 🛒</a>
